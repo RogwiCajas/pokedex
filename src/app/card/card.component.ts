edd0f9b9f71833b99661fe94ddbbba59f3a4ce6d
RogwiCajas <rogwinalex2@hotmail.com>
2022-02-23 09:32:59 -0500

    type error solved

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { PokemonService } from '../services/pokemon.service';
+import {IPokemon} from '../interface/pokemon.interface';
 
 
 @Component({
@@ -10,10 +11,30 @@ import { PokemonService } from '../services/pokemon.service';
 })
 export class CardComponent implements OnInit {
   //@Input() nombre = '';
-  @Input() pokemon: object = {};
+  @Input() pokemon:IPokemon= {
+    abilities: [],
+    base_experience: 0,
+    forms: [],
+    game_indices: [],
+    height: 0,
+    held_items: [],
+    id: 0,
+    is_default: false,
+    location_area_encounters: '',
+    moves: [],
+    name: '',
+    order: 0,
+    past_types: [],
+    species: {},
+    sprites: {},
+    stats: [],
+    types: [],
+    weight: 0
+
+  };
    
   constructor(
-    private pokemonService: PokemonService
+    private pokemonService: PokemonService 
   ) { 
     
   }
